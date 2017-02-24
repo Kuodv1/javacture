a88777b5aecc0db65b70ffce08df3f286f7874cf
Goketsu <benjamin.viraize@gmail.com>
2017-02-24 17:45:48 +0100

    leger changement

--- Javacture/src/controller/ControllerImageDeux.java
+++ Javacture/src/controller/ControllerImageDeux.java
@@ -173,7 +173,7 @@ public class ControllerImageDeux {
         
         ControllerEditableAgencement cea = fxmlLoader.getController();
         cea.setCadreImageToEdit(_cadreImage);
-        Scene scene = new Scene(root, 200, 100);
+        Scene scene = new Scene(root, 240, 100);
         stage.setScene(scene);
         stage.show();
 	}
