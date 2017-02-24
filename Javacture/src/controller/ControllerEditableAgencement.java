5ea2ae5b94777128ed0c2cac7d865022d9dd9a14
Kuodv1 <louismougin54@gmail.com>
2017-02-24 17:58:58 +0100

    Commit ajoutEcouteur d'édition

--- Javacture/src/controller/ControllerEditableAgencement.java
+++ Javacture/src/controller/ControllerEditableAgencement.java
@@ -1,5 +1,9 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
 
 public class ControllerEditableAgencement {
@@ -7,15 +11,76 @@ public class ControllerEditableAgencement {
 	
 	private Pane cadreImageToEdit;
 	
+	private final String stringBackgroundColor =  "-fx-background-color: ";
+	private String fontColor = "white";
+	
+	private final String stringBorderColor = "-fx-border-color: ";
+	private String borderColor = "blue";
+	
+	
+	@FXML
+	Slider _sliderWidthBorder;
+	
+	private final String stringBorderWidth = "-fx-border-width: ";
+	private String widthBorder = "1";
+	
 	public ControllerEditableAgencement() {
 		
 	}
 	
 	public void initialize() {
 		System.out.println("Initialisze ControllerEditableAgencement Check !");
+		initialize_slider_width_border();
+	}
+	
+	public void reloadParam(String style) {
+		int startBackgrounColor = style.indexOf(stringBackgroundColor); 
+		if(startBackgrounColor>=0)
+			fontColor = style.substring(startBackgrounColor+stringBackgroundColor.length(), style.indexOf(';', startBackgrounColor));
+		
+		int startBorderColor = style.indexOf(stringBorderColor);
+		if(startBorderColor>=0)
+			borderColor = style.substring(startBorderColor+stringBorderColor.length(), style.indexOf(';', startBorderColor));
+		
+		int startBorderWidth = style.indexOf(stringBorderWidth);
+		if(startBorderWidth>=0)
+			widthBorder = style.substring(startBorderWidth+stringBorderWidth.length(), style.indexOf(';', startBorderWidth));
+		
 	}
 	
 	public void setCadreImageToEdit(Pane cadreImageToEdit) {
 		this.cadreImageToEdit = cadreImageToEdit;
+		
+		this.reloadParam(cadreImageToEdit.getStyle());
+		
+		/*cadreImageToEdit.styleProperty().setValue(" -fx-border-color: "+"blue"+";"
+				+ "-fx-border-width: "+5+";");*/
+		
+		System.out.println(cadreImageToEdit.getStyle());
+				
+	}
+	
+	
+	public void initialize_slider_width_border() {
+		_sliderWidthBorder.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+                    changeWidthBorder(new_val.intValue());
+            }
+        });
+	}
+	
+	
+	public void changeWidthBorder(int widthBorderInt){
+		this.widthBorder = ""+widthBorderInt;
+		applyStyle();
+	}
+	
+	public void applyStyle() {
+		cadreImageToEdit.styleProperty().setValue("-fx-background-color: "+fontColor+";"+
+				" -fx-border-color: "+borderColor+";"
+				+ "-fx-border-width: "+widthBorder+";");
+		System.out.println("apply?");
+		System.out.println(cadreImageToEdit.getStyle());
 	}
 }
